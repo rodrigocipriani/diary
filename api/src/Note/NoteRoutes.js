8fd01a390cbef74eae0d5d41a0395c2930719bcf
Rodrigo Cipriani da Rosa <chopsss@gmail.com>
2018-02-03 17:55:22 -0200

    Protect route noteadd

--- api/src/Note/NoteRoutes.js
+++ api/src/Note/NoteRoutes.js
@@ -1,4 +1,8 @@
+const passport = require('passport');
 const NoteController = require('./NoteController');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+// const requireSignin = passport.authenticate('local', { session: false });
 
 const API_PREFIX = '/note';
 
@@ -6,6 +10,6 @@ module.exports = (app) => {
   // app.post(`${API_PREFIX}/signin`, requireSignin, AuthController.signin);
 
   app.get(`${API_PREFIX}/list`, NoteController.list);
-  app.get(`${API_PREFIX}/add`, NoteController.add);
+  app.get(`${API_PREFIX}/add`, requireAuth, NoteController.add);
 };
 
